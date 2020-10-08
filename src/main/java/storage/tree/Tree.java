05aa59091c58fe936f3c29549d60d69e984d2943
zZooMm33 <36926228+zZooMm33@users.noreply.github.com>
2020-10-08 13:29:46 +0300

    Начало, создан необходимый минимум файлов. Сделать:  - получение файла  - чтение в дерево  - вывод на экран  - запись в файл

---
+++ src/main/java/storage/tree/Tree.java
@@ -0,0 +1,83 @@
+package storage.tree;
+
+public class Tree {
+    private int nodeId;
+    private int parentId;
+    private String nodeName;
+    private String nodeValue;
+
+    public int getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(int nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getNodeName() {
+        return nodeName;
+    }
+
+    public void setNodeName(String nodeName) {
+        this.nodeName = nodeName;
+    }
+
+    public String getNodeValue() {
+        return nodeValue;
+    }
+
+    public void setNodeValue(String nodeValue) {
+        this.nodeValue = nodeValue;
+    }
+
+    public static final class TreeBuilder {
+        private int nodeId;
+        private int parentId;
+        private String nodeName;
+        private String nodeValue;
+
+        private TreeBuilder() {
+        }
+
+        public static TreeBuilder aTree() {
+            return new TreeBuilder();
+        }
+
+        public TreeBuilder withNodeId(int nodeId) {
+            this.nodeId = nodeId;
+            return this;
+        }
+
+        public TreeBuilder withParentId(int parentId) {
+            this.parentId = parentId;
+            return this;
+        }
+
+        public TreeBuilder withNodeName(String nodeName) {
+            this.nodeName = nodeName;
+            return this;
+        }
+
+        public TreeBuilder withNodeValue(String nodeValue) {
+            this.nodeValue = nodeValue;
+            return this;
+        }
+
+        public Tree build() {
+            Tree tree = new Tree();
+            tree.setNodeId(nodeId);
+            tree.setParentId(parentId);
+            tree.setNodeName(nodeName);
+            tree.setNodeValue(nodeValue);
+            return tree;
+        }
+    }
+}
